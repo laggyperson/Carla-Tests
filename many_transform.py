bd62e275b4a6d231037fdbb4cce24532b730be5d
Phillip Chen <phillipe10042003@berkeley.edu>
2023-04-18 22:56:16 -0700

    The transform working perfectly fine, even under extreme loads

--- many_transform.py
+++ many_transform.py
@@ -39,47 +39,65 @@ def main():
     number_npcs = args.n
     
 	# Accessing Carla server
-	client = carla.Client('localhost', 2000)
-	client.set_timeout(4.0)
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(4.0)
 
-	# Load in world
-	world = client.get_world()
+    # Load in world
+    world = client.get_world()
+    
+    # Turning on Synchronous mode to control tick of world
+    settings = world.get_settings()
+    settings.synchronous_mode = True
 
-	# Get blueprints and choose one for the NPCs
-	blueprint_library = world.get_blueprint_library()
-	npc_bp = blueprint_library['cybertruck']
+    # Get blueprints and choose one for the NPCs
+    blueprint_library = world.get_blueprint_library()
+    npc_bp = random.choice(blueprint_library.filter('cybertruck'))
 
     # Spawning NPCs in a row
     spawn_point = random.choice(world.get_map().get_spawn_points())
+    spawn_point.location.z += 50
     for i in range(number_npcs):
-        spawn_curr = spawn_point.location.x + (i * 4 - number_npcs / 2)
-        npc = world.spawn(npc_bp, spawn_curr)
-        actors.append(npc)
+        spawn_curr = random.choice(world.get_map().get_spawn_points())
+        spawn_curr.location.x = spawn_point.location.x + (i - number_npcs / 2) * 10
+        spawn_curr.location.y = spawn_point.location.y
+        spawn_curr.location.z = spawn_point.location.z
+        spawn_curr.rotation = spawn_point.rotation
+        npc = world.try_spawn_actor(npc_bp, spawn_curr)
+        if not (npc == None):
+            npc.set_simulate_physics(False)
+            print("Spawned actor {} at: x: {}, y: {}, z: {}".format(i, spawn_curr.location.x, spawn_curr.location.y, spawn_curr.location.z))
+            actors.append(npc)
+    
+    num_spawned = len(actors) # Keeps track of how many vehicles actually spawned
 
     # Moving NPCs by a little bit forwards
     while True:
         time.sleep(0.05) # The step length of the simulation in Sumo
 
         # Updating all NPC locations
+        print("############################ MOVING {} ACTORS #################################".format(num_spawned))
         for c in actors:
             curr_pos = c.get_transform()
             curr_pos.location.y += 3
             c.set_transform(curr_pos)
-
+            print("Moved NPC to: x: {}, y: {}, z: {}".format(spawn_curr.location.x, spawn_curr.location.y, spawn_curr.location.z))
         # Ticking the world
         world.tick()
 
 
 if __name__ == '__main__':
-	actors = []
-	try:
-		main()
-	except KeyboardInterrupt:
-		print('\nCancelled by user. Bye!')
-	except RuntimeError as e:
-		print(e)
-	finally:
-		print("Destroying all actors")
-		for actor in actors:
-			actor.destroy()
-		print("Finished!")
+    actors = []
+    try:
+        time_start = time.time()
+        main()
+    except KeyboardInterrupt:
+        print('\nCancelled by user. Bye!')
+    except RuntimeError as e:
+        print(e)
+    finally:
+        time_end = time.time()
+        print("Time elapsed: ", time_end - time_start)
+        print("Destroying {} actors".format(len(actors)))
+        for actor in actors:
+            actor.destroy()
+        print("Finished!")
